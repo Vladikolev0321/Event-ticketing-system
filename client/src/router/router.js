c96bc0e0a55a169895049e671d93f1e8290113d7
Vladislav Kolev <vladi.kolev2003@gmail.com>
2022-08-12 16:33:12 +0300

    feat: Add marketplace and myTickets routes

--- client/src/router/router.js
+++ client/src/router/router.js
@@ -2,6 +2,8 @@ import { createRouter, createWebHistory } from 'vue-router'
 import EventsView from '../views/EventsView.vue'
 import CreateEvent from '../views/CreateEvent.vue'
 import MyEvents from '../views/MyEvents.vue'
+import MarketPlace from '../views/Marketplace.vue'
+import MyTickets from '../views/MyTickets.vue'
 
 const routes = [
     { path: '/', redirect: '/home' },
@@ -16,6 +18,14 @@ const routes = [
     {
       path: '/myEvents',
       component: MyEvents,
+    },
+    {
+      path: '/market',
+      component: MarketPlace,
+    },
+    {
+      path: '/myTickets',
+      component: MyTickets,
     }
 ];
 
