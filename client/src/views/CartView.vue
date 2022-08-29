341d01ddffab13a1b09cd8a812e721604b68cf38
Vladislav Kolev <vladi.kolev2003@gmail.com>
2022-08-29 18:30:09 +0300

    feat: Add cart view

---
+++ client/src/views/CartView.vue
@@ -0,0 +1,143 @@
+<template>
+  <h1>Cart</h1>
+  <div class="checkout-box">
+    <ul class="checkout-list">
+      <transition-group name="fade">
+        <li
+          v-for="(product, index) in getProductsInCart()"
+          :key="index"
+          class="checkout-product"
+        >
+          <img :src="product.image" alt="" class="product-image" />
+          <h3 class="product-name">{{ product.name }}</h3>
+          <span class="product-price">R$ {{ product.price }},00 </span>
+          <button class="product-remove" @click="remove(index)">X</button>
+        </li>
+      </transition-group>
+    </ul>
+    <div v-if="!hasProduct()" class="checkout-message">
+      <h3>No products...</h3>
+      <router-link to="./">Back to list of products</router-link>
+    </div>
+    <h3 class="total" v-if="hasProduct()">Total: R$ {{ totalPrice() }}, 00</h3>
+    <MDBBtn tag="a" color="primary" href="https://mdbootstrap.com/">Buy</MDBBtn>
+  </div>
+</template>
+
+<script>
+import {
+  MDBBtn,
+}
+from "mdb-vue-ui-kit";
+
+export default {
+  name: 'CartView',
+  components: {
+    MDBBtn,
+  },
+  data() {
+    return {
+      products: [
+        {
+          id: 1,
+          name: 'Ticket 1',
+          price: 100,
+          image: 'https://mdbootstrap.com/img/new/standard/nature/184.webp',
+          inCart: true,
+        },
+        {
+          id: 2,
+          name: 'Ticket 2',
+          price: 200,
+          image: 'https://mdbootstrap.com/img/new/standard/nature/184.webp',
+          inCart: true,
+        }
+      ],
+    };
+  },
+  methods: {
+    getProductsInCart() {
+      return this.products.filter(product => product.inCart);
+    },
+    hasProduct() {
+      return this.getProductsInCart().length > 0;
+    },
+    totalPrice() {
+      return this.getProductsInCart().reduce((total, product) => {
+        return total + product.price;
+      }, 0);
+    },
+    remove(index) {
+      this.products.splice(index, 1);
+    },
+  },
+  created() {
+    // this.products = this.$store.state.products;
+  },
+};
+</script>
+
+<style scoped>
+
+.checkout-box {
+    width: 100%;
+    max-width: 900px;
+    display: flex;
+    flex-direction: column;
+    margin: 50px auto;
+    box-sizing: border-box;
+    padding: 1em;
+  }
+  .checkout-list {
+    padding: 0;
+  }
+  .checkout-product {
+    display: grid;
+    grid-template-columns: 1fr 3fr 2fr .5fr;
+    background-color: #fff;
+    box-shadow: 0px 0px 10px rgba(73, 74, 78, 0.1);
+    border-radius: 5px;
+    list-style: none;
+    box-sizing: border-box;
+    padding: .8em;
+    margin: 1em 0;
+  }
+  .checkout-product * {
+    place-self: center;
+  }
+  .product-image {
+    grid-column: 1/2;
+    width: 50%;
+  }
+  .product-name {
+    box-sizing: border-box;
+  }
+  .product-price {
+    font-size: 1.2em;
+    font-weight: bold;
+  }
+  .product-remove {
+    width: 25px;
+    height: 25px;
+    border-radius: 50%;
+    border: 0;
+    background-color: #E0E0E0;
+    color: #fff;
+    cursor: pointer;
+  }
+  .total {
+    font-size: 2em;
+    font-weight: bold;
+    align-self: flex-end;
+  }
+  .checkout-message {
+    font-size: 1.5em;
+  }
+  .fade-enter-active, .fade-leave-active {
+    transition: all .5s;
+  }
+  .fade-enter, .fade-leave-to {
+    transform: translateX(-40px);
+    opacity: 0;
+  }
+</style>
