dc3e0fceb45e227ca7b634b071ba2d27b154f080
Vladikolev0321 <vladi.kolev2003@gmail.com>
2022-09-22 11:28:12 +0300

    feat: Add function to connect to smart contracts at the start of the app

--- client/src/App.vue
+++ client/src/App.vue
@@ -13,11 +13,38 @@
 
 <script>
 import NavBar from './components/NavBar.vue';
+import DEX from '../../../Event-ticketing-system-smart-contracts/Event-ticketing-system/build/contracts/DEX.json';
+import EventManager from '../../../Event-ticketing-system-smart-contracts/Event-ticketing-system/build/contracts/EventManager.json';
+
 export default {
   name: 'App',
   components: {
     NavBar,
-}
+  },
+  methods: {
+    async connectToContracts() {
+      const Web3 = require("web3");
+      const web3 = await new Web3(window.ethereum);
+      const eventManager = new web3.eth.Contract(
+        EventManager.abi,
+        "",
+      );
+      const dex = new web3.eth.Contract(
+        DEX.abi,
+        "",//address of DEX
+      );
+      this.$store.commit('setEventManager', eventManager);
+      this.$store.commit('setDex', dex);
+    },
+    loadEvents() {
+      this.$store.state.eventManager.getEvents().then((events) => { // to add method to smart contract
+        this.$store.commit('setEvents', events);
+      }); 
+    },
+  },
+  beforeMount() {
+    this.connectToContracts();
+  },
 }
 </script>
 
