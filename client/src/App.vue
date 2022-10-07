a12014f2aa4e902084615f06186e3b44264f4183
Vladikolev0321 <vladi.kolev2003@gmail.com>
2022-10-07 11:29:49 +0300

    feat: Add connection to contracts functionality

--- client/src/App.vue
+++ client/src/App.vue
@@ -15,6 +15,10 @@
 import NavBar from './components/NavBar.vue';
 import DEX from '../../../Event-ticketing-system-smart-contracts/Event-ticketing-system/build/contracts/DEX.json';
 import EventManager from '../../../Event-ticketing-system-smart-contracts/Event-ticketing-system/build/contracts/EventManager.json';
+// import Moralis from 'moralis';
+const Moralis = require("moralis-v1");
+// import Moralis from 'moralis/dist/moralis.min.js';
+
 
 export default {
   name: 'App',
@@ -27,7 +31,7 @@ export default {
       const web3 = await new Web3(window.ethereum);
       const eventManager = new web3.eth.Contract(
         EventManager.abi,
-        "",
+        "0x1A381048884a238c6C59fef1f5Fe2ba1D9A367AF",
       );
       const dex = new web3.eth.Contract(
         DEX.abi,
@@ -36,14 +40,61 @@ export default {
       this.$store.commit('setEventManager', eventManager);
       this.$store.commit('setDex', dex);
     },
-    loadEvents() {
-      this.$store.state.eventManager.getEvents().then((events) => { // to add method to smart contract
-        this.$store.commit('setEvents', events);
-      }); 
+    async loadEvents() {
+      const eventsCount = this.$store.state.eventManager.methods.eventsCount().call();
+      let events = [];
+      for(let i = 1; i <= eventsCount; i++) {
+        const event = await this.$store.state.eventManager.methods.getEvent(i).call();
+        const uri = await this.$store.state.eventManager.methods.uri(i).call();
+        const response = await fetch(uri);
+        const metadata = await response.json();
+        
+        events.push({
+          id: event[0],
+          name: metadata.name,
+          description: metadata.description,
+          tickets: metadata.tickets, // ? get from contract
+          // ticketsSold: event[5],
+          // owner: event[6],
+        });
+      }
+      this.$store.commit('setEvents', events);
     },
+    async loadTicketsForSale() {
+      const ticketsCount = await this.$store.state.dex.methods.listingCount().call();
+      let tickets = [];
+      for(let i = 1; i <= ticketsCount; i++) {
+        const listing = await this.$store.state.dex.methods.getListing(i).call();
+        if(listing.status === '0'){
+          const uri = await this.$store.state.eventManager.methods.uri(listing.tokenId).call();
+          const response = await fetch(uri);
+          const metadata = await response.json();
+          tickets.push({
+            id: listing.tokenId,
+            name: metadata.name,
+            description: metadata.description,
+            price: metadata.price,
+            // owner: ticket[4],//
+          });
+        }
+      }
+      this.$store.commit('setTicketsForSale', tickets);
+    },
+  //start moralis
+  async loadMoralis() {
+    let serverUrl = "https://rfwik76n3pdy.usemoralis.com:2053/server";
+    let appId = "Yu28U62SBduQbzSQztPBMWk7sNlRs5T9wsC8Lqb3";
+    console.log(Moralis)
+    Moralis.start({ serverUrl: serverUrl, appId: appId });
+    await Moralis.authenticate();
+    this.$store.commit('setMoralis', Moralis);
+  },
   },
   beforeMount() {
     this.connectToContracts();
+    // this.loadEvents();
+    // this.loadTicketsForSale();
+    this.loadMoralis();
   },
 }
 </script>
